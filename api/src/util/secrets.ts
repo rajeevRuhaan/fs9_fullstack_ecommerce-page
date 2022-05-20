cae36c24fda1061262ad1dbb8894eb13eb917d13
Rajeev <s2100155@edu.bc.fi>
2022-05-20 09:32:31 +0300

    secrets.ts in api .env in if block

--- api/src/util/secrets.ts
+++ api/src/util/secrets.ts
@@ -5,14 +5,16 @@ import logger from './logger'
 
 if (fs.existsSync('.env')) {
   logger.debug('Using .env file to supply config environment variables')
-  dotenv.config({ path: '.env' })
+  if ('.env') {
+    dotenv.config({ path: '.env' })
+  }
 } else {
   logger.debug('Using .env.example file to supply config environment variables')
 }
 
 export const ENVIRONMENT = process.env.NODE_ENV
 const prod = ENVIRONMENT === 'production' // Anything else is treated as 'dev'
-
+console.log('process.env', process.env)
 export const GOOGLE_CLIENT_ID = process.env['GOOGLE_CLIENT_ID'] as string
 export const JWT_SECRET = process.env['JWT_SECRET'] as string
 export const MONGODB_URI = (
