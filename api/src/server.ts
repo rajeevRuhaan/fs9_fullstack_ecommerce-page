6fe448fc80be73a0bb1d25c0e5138b9834969b45
Rajeev <s2100155@edu.bc.fi>
2022-06-02 12:55:41 +0300

    something

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,16 @@
 import errorHandler from 'errorhandler'
 import mongoose from 'mongoose'
+// import dotenv from 'dotenv'
 
 import app from './app'
 import { MONGODB_URI } from './util/secrets'
 import logger from './util/logger'
 
+// dotenv.config({ path: '.env' })
+
 const mongoUrl = MONGODB_URI
-console.log('mongo UR L:', mongoUrl)
+
+// const mongo = `mongodb://fs9-fullstack:${process.env.PASSWORD}@cluster0-shard-00-00.kienc.mongodb.net:27017,cluster0-shard-00-01.kienc.mongodb.net:27017,cluster0-shard-00-02.kienc.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-bq83u0-shard-0&authSource=admin&retryWrites=true&w=majority`
 mongoose
   .connect(mongoUrl, {
     useNewUrlParser: true,
